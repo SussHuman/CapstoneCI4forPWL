57de9f7e8477635e00b5750b140f951fdb3f2a42
SussHuman <adventurelol1234@gmail.com>
2026-05-28 15:48:31 +0700

    28 Mei 2026 - Data Edit/Create/Delete

---
+++ app/Controllers/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ProfileController extends BaseController
+{
+    public function index()
+    {
+        $data = [
+            'username' => session()->get('username'),
+            'email' => session()->get('email'),
+            'role' => session()->get('role'),
+            'login_time' => session()->get('login_time'),
+            'status' => session()->get('status'),
+        ];
+        return view('v_profile', $data);
+    }
+}
